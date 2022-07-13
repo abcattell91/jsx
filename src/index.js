cbd69f57e6a0f91e4b9af8ed0f3ad44186def2e8
Alex Cattell <abcattell91@gmail.com>
2022-07-13 16:33:51 +0100

    setup index.js file

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 // 2) Get a reference to the div with ID root
+const el = document.getElementById('root');
 
 // 3) Tell React to take control of that element
-
-// 4) Create a component with
+const root = ReactDOM.createRoot(el);
+// 4) Create a component
+function App() {
+  return <h1>Hi There!</h1>
+}
 // 5) Show the component on the screen
+root.render(<App />);
